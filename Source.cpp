808906bace1ffde16b806df2d91fd97b637b61a4
Aidar Asf <79790554+Sazerfai@users.noreply.github.com>
2023-09-14 20:55:46 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -6,116 +6,259 @@
 #include "Camera.h"
 
 using namespace sf;
+using namespace std;
 
 
-class Player { // класс Игрока
-private: float x, y;//объявили переменные, в конструкторе Player ниже дадим им значения
+class Map  {
 public:
-	float w, h, dx, dy, speed;
-	int dir; 
-	String File;
-	Image image;//сфмл изображение
-	Texture texture;//сфмл текстура
-	Sprite sprite;//сфмл спрайт
-	Player(String F, float X, float Y, float W, float H) { 
-		dx = 0;dy = 0;speed = 0;dir = 0;
-		File = F;
-		w = W; h = H;
-		image.loadFromFile("D:\\" + File);
-		image.createMaskFromColor(Color::Black);
+	String TileMap[19] = {
+	"                                                                        c                 ",
+	"               c                                   c                                 c    ",
+	"      c                                  c                      c        c                ",
+	"                         c                                                                ",
+	"          c                                        c                                      ",
+	"                        c                                                        c        ",
+	"      c       c                 c              c                 c                        ",
+	"                                   m    e                                                 ",
+	"                                   0000000000000000                                       ",
+	"                                   0                                                      ",
+	"                                   0              m                                       ",
+	"0                                  0     00000000000                                     0",
+	"0                                   0          0                                         0",
+	"0                                    0  m      0                                         0",
+	"0                                    00000     0                                         0",
+	"0                0           0000             0                                          0",
+	"0               0           0           m    0                                           0",
+	"0 t       t   0   e      s0    t 000000000000 m                                          0",
+	"000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+	};
+};
+
+class Object {
+public:
+	float dx, dy, x, y, speed;
+	int w, h, health;
+	bool life, isMove, onGround;
+	Texture texture;
+	Sprite sprite;
+	String name;
+	Object(Image& image, float X, float Y, int W, int H, String Name) {
+		x = X; y = Y; w = W; h = H; name = Name; 
+		speed = 0; health = 3; dx = 0; dy = 0;
+		life = true; onGround = false; isMove = false;
 		texture.loadFromImage(image);
 		sprite.setTexture(texture);
-		x = X; y = Y;
-		sprite.setTextureRect(IntRect(1320, 0, 170, 210));
+		sprite.setOrigin(w / 2, h / 2);
 	}
-	float getplayercoordinateX() {	//этим методом будем забирать координату Х	
+	~Object();
+};
+/////////////////////////////РљР›РђРЎРЎ РР“Р РћРљРђ////////////////////////
+class Player :public Object {
+public:
+	enum { left, right, up, down, jump, stay } state;// С‚РёРї РїРµСЂРµС‡РёСЃР»РµРЅРёСЏ - СЃРѕСЃС‚РѕСЏРЅРёРµ РѕР±СЉРµРєС‚Р°
+	int playerScore;
+	Player(Image& image, float X, float Y, int W, int H, String Name) :Object(image, X, Y, W, H, Name) {
+		playerScore = 0; state = stay;
+		float currentFrame = 0;
+
+		if (name == "Player1") {
+			sprite.setTextureRect(IntRect(0, 0, w + 120, 210));
+		}
+	}
+	~Player();
+	void control() {
+		if (Keyboard::isKeyPressed) {
+			if (Keyboard::isKeyPressed(Keyboard::Left)) {
+				state = left; speed = 0.1;
+			}
+			if (Keyboard::isKeyPressed(Keyboard::Right)) {
+				state = right; speed = 0.1;
+			}
+
+			if ((Keyboard::isKeyPressed(Keyboard::Up)) && (onGround)) {//РµСЃР»Рё РЅР°Р¶Р°С‚Р° РєР»Р°РІРёС€Р° РІРІРµСЂС… Рё РјС‹ РЅР° Р·РµРјР»Рµ, С‚Рѕ РјРѕР¶РµРј РїСЂС‹РіР°С‚СЊ
+				state = jump; dy = -0.6; onGround = false;
+			}
+
+			if (Keyboard::isKeyPressed(Keyboard::Down)) {
+				state = down;
+			}
+		}
+	}
+	void update(float time, Map carta)
+	{
+		control();
+		switch (state)
+		{
+		case right: dx = speed;break;//СЃРѕСЃС‚РѕСЏРЅРёРµ РёРґС‚Рё РІРїСЂР°РІРѕ
+		case left: dx = -speed;break;//СЃРѕСЃС‚РѕСЏРЅРёРµ РёРґС‚Рё РІР»РµРІРѕ
+		case up: break;
+		case down: break;
+		case jump: break;
+		case stay: break;
+		}
+		x += dx * time;
+		checkCollisionWithMap(dx, 0, carta);//РѕР±СЂР°Р±Р°С‚С‹РІР°РµРј СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РїРѕ РҐ
+		y += dy * time;
+		checkCollisionWithMap(0, dy, carta);//РѕР±СЂР°Р±Р°С‚С‹РІР°РµРј СЃС‚РѕР»РєРЅРѕРІРµРЅРёРµ РїРѕ Y
+		if (!isMove) speed = 0;
+		sprite.setPosition(x, y);
+		if (health <= 0) { life = false; }
+		dy = dy + 0.0015 * time;
+	}
+
+	float getplayercoordinateX() {
 		return x;
 	}
-	float getplayercoordinateY() {	//этим методом будем забирать координату Y 	
+	float getplayercoordinateY() {
 		return y;
+	}
+
+	void checkCollisionWithMap(float Dx, float Dy, Map carta)//С„ С†РёСЏ РїСЂРѕРІРµСЂРєРё СЃС‚РѕР»РєРЅРѕРІРµРЅРёР№ СЃ РєР°СЂС‚РѕР№
+	{
+		for (int i = y / 32; i < (y + h) / 32; i++)//РїСЂРѕС…РѕРґРёРјСЃСЏ РїРѕ СЌР»РµРјРµРЅС‚Р°Рј РєР°СЂС‚С‹
+			for (int j = x / 32; j < (x + w) / 32; j++)
+			{
+				if (carta.TileMap[i][j] == '0')
+				{
+					if (Dy > 0) { y = i * 32 - h;  dy = 0; onGround = true; }//РїРѕ Y РІРЅРёР·=>РёРґРµРј РІ РїРѕР»(СЃС‚РѕРёРј РЅР° РјРµСЃС‚Рµ) РёР»Рё РїР°РґР°РµРј. Р’ СЌС‚РѕС‚ РјРѕРјРµРЅС‚ РЅР°РґРѕ РІС‹С‚РѕР»РєРЅСѓС‚СЊ РїРµСЂСЃРѕРЅР°Р¶Р° Рё РїРѕСЃС‚Р°РІРёС‚СЊ РµРіРѕ РЅР° Р·РµРјР»СЋ, РїСЂРё СЌС‚РѕРј РіРѕРІРѕСЂРёРј С‡С‚Рѕ РјС‹ РЅР° Р·РµРјР»Рµ С‚РµРј СЃР°РјС‹Рј СЃРЅРѕРІР° РјРѕР¶РµРј РїСЂС‹РіР°С‚СЊ
+					if (Dy < 0) { y = i * 32 + 32;  dy = 0; }
+					if (Dx > 0) { x = j * 32 - w; }
+					if (Dx < 0) { x = j * 32 + 32; }
+				}
+				if (carta.TileMap[i][j] == 'm') {
+					carta.TileMap[i][j] = ' ';
+					playerScore = playerScore + 50;
+				}
+				if (carta.TileMap[i][j] == 'e') {
+					carta.TileMap[i][j] = ' ';
+					health = health - 1;
+				}
+				if (carta.TileMap[i][j] == 's') {
+					carta.TileMap[i][j] = ' ';
+					playerScore = playerScore + 50;
+					health = health - 1;
+				}
+
+			}
 	}
 };
 
 
 int main()
+{
+
+
+	RenderWindow window(VideoMode(1216, 608), "Super Mario Bros.");
+	view.reset(FloatRect(0, 0, 1216, 608));
+
+	Font font;
+	font.loadFromFile("C:\\Users\\RedNewty\\Desktop\\Course work\\Code\\Font.ttf");
+	Text text("", font, 22);
+	Font font1;
+	font1.loadFromFile("C:\\Users\\RedNewty\\Desktop\\Course work\\Code\\Font.ttf");
+	Text text1("", font1, 60);
+	Image map_image;
+	map_image.loadFromFile("C:\\Users\\RedNewty\\Desktop\\Course work\\Code\\f_map.png");
+	Texture map;
+	map.loadFromImage(map_image);
+	Sprite s_map;
+	s_map.setTexture(map);
+
+	Image heroImage;
+	heroImage.loadFromFile("C:\\Users\\RedNewty\\Desktop\\Course work\\Code\\hero1.png");
+	heroImage.createMaskFromColor(Color::Black);
+
+	Player p(heroImage, 100, 502.5, 50, 60, "Player1");
+	p.sprite.scale(0.35, 0.35);
+	Map carta;
+	
+	float currentFrame = 0;
+	Clock clock;
+	float dX = 0;
+	float dY = 0;
+	Clock gameTimeClock;//РїРµСЂРµРјРµРЅРЅР°СЏ РёРіСЂРѕРІРѕРіРѕ РІСЂРµРјРµРЅРё
+	int gameTime = 0;
+	float CurrentFrame = 0;
+
+	while (window.isOpen())
 	{
-		srand(static_cast<unsigned int>(time(0)));
-		RenderWindow window(VideoMode(1200, 600), "Super Mario Bros.");
-		view.reset(sf::FloatRect(0, 0, 1200, 600));
+		if (p.life) gameTime = gameTimeClock.getElapsedTime().asSeconds(); 
+		float time = clock.getElapsedTime().asMicroseconds();
 
-		Texture texture1;
-		texture1.loadFromFile("D:\\background.jpg");
-		Sprite backgroundsprite(texture1);
+		clock.restart();
+		time = time / 800;
 
-		Texture texture2;
-		texture2.loadFromFile("D:\\ground.png");
-		Sprite groundsprite(texture2);
 
-		groundsprite.scale(0.2, 0.2);
 
-		Player p("mario.png", 1320, 0, 170, 210);
-
-		//Texture herotexture;//создаем объект Texture (текстура)
-		//herotexture.loadFromFile("C:\\Users\\RedNewty\\Desktop\\Course work\\Code\\hero.png");//загружаем картинку
-		//Sprite herosprite(herotexture);//создаем объект Sprite(спрайт)
-		//herosprite.setTextureRect(IntRect(560, 0, 137.5, 160));
-
-		p.sprite.scale(0.35, 0.35);
-		p.sprite.setPosition(100, 475);
-		Clock clock;
-		float time = clock.getElapsedTime().asMicroseconds(); //дать прошедшее время в микросекундах
-		clock.restart(); //перезагружает время
-
-		time = time / 8000; //скорость игры
-		//float CurrentFrame = 170;
-		
-		while (window.isOpen())
+		Event event;
+		while (window.pollEvent(event))
 		{
-			sf::Event event;
-			while (window.pollEvent(event))
-			{
-
-				if (event.type == sf::Event::Closed)
-					window.close();
-			}
-
-			if (sf::Keyboard::isKeyPressed(Keyboard::Left)) {
-				
-				p.sprite.move(-90 * time, 0);
-				p.sprite.setTextureRect(IntRect(1090, 0, 170, 210));
-				getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
-
-			}
-			if (sf::Keyboard::isKeyPressed(Keyboard::Right)) { 
-				p.sprite.move(90 * time, 0);
-				p.sprite.setTextureRect(IntRect(1320, 0, 170, 210));
-				getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());//передаем координаты игрока в функцию управления камерой
-
-			}
-			window.setView(view);
-			window.clear();
-
-			window.draw(backgroundsprite);
-			groundsprite.setPosition(0, 550);
-			window.draw(groundsprite);
-			groundsprite.setPosition(400, 550);
-			window.draw(groundsprite);
-			groundsprite.setPosition(800, 550);
-			window.draw(groundsprite);
-			
-			//if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Up) { 
-			//	CurrentFrame += 0.005 * time; //служит для прохождения по "кадрам". переменная доходит до трех суммируя произведение времени и скорости. изменив 0.005 можно изменить скорость анимации
-			//	if (CurrentFrame > 4) CurrentFrame -= 4; //проходимся по кадрам с первого по третий включительно. если пришли к третьему кадру - откидываемся назад.
-			//	p.sprite.setTextureRect(IntRect(1320 * int(CurrentFrame), 0, 170, 210)); //проходимся по координатам Х. получается 96,96*2,96*3 и опять 96
-			//	p.sprite.move(-0.1 * time, 0);//происходит само движение персонажа влево
-			//	p.sprite.move(0, -0.1 ); }
-			//if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::Down) { p.sprite.move(0, 0.1); }
-
-			window.draw(p.sprite);
-
-			window.display();
+			if (event.type == sf::Event::Closed)
+				window.close();
 		}
 
-		return 0;
+/////////////РЈРїСЂР°РІР»РµРЅРёРµ РїРµСЂСЃРѕРЅР°Р¶РµРј СЃ Р°РЅРёРјР°С†РёРµР№/////////////
+{
+			getplayercoordinateforview(p.getplayercoordinateX(), p.getplayercoordinateY());
+			if (sf::Keyboard::isKeyPressed(Keyboard::Left)) {
+				p.state = p.right; p.speed = 120;
+				CurrentFrame += time * 0.005;
+				if (CurrentFrame > 1.5) CurrentFrame -= 1;
+				p.sprite.setTextureRect(IntRect(170 * int(CurrentFrame), 210, 170, 210));
+
+			}
+			if (sf::Keyboard::isKeyPressed(Keyboard::Right)) {
+				p.state = p.left; p.speed = 120;
+				CurrentFrame += time * 0.005;
+				if (CurrentFrame > 1.5) CurrentFrame -= 1;
+				p.sprite.setTextureRect(IntRect(170 * int(CurrentFrame), 0, 170, 210));
+			}
+			if (sf::Keyboard::isKeyPressed(Keyboard::Up)) {
+				p.state = p.up; p.speed = 120;
+				p.sprite.setTextureRect(IntRect(0, 420, 170, 210));
+			}
+		}
+
+		p.update(time, carta);
+		window.setView(view);
+		window.clear();
+
+		for (int i = 0; i < 19; i++)
+			for (int j = 0; j < 91; j++)
+			{
+				if (carta.TileMap[i][j] == ' ')  s_map.setTextureRect(IntRect(64, 0, 32, 32)); 
+				if (carta.TileMap[i][j] == '0')  s_map.setTextureRect(IntRect(96, 0, 32, 32));  
+				if ((carta.TileMap[i][j] == 'c')) s_map.setTextureRect(IntRect(32, 0, 32, 32)); 
+				if ((carta.TileMap[i][j] == 't')) s_map.setTextureRect(IntRect(0, 0, 32, 32));  
+				if ((carta.TileMap[i][j] == 'm')) s_map.setTextureRect(IntRect(128, 0, 32, 32));
+				if ((carta.TileMap[i][j] == 's')) s_map.setTextureRect(IntRect(160, 0, 32, 32));
+				if ((carta.TileMap[i][j] == 'e')) s_map.setTextureRect(IntRect(192, 0, 32, 32));
+
+				s_map.setPosition(j * 32, i * 32);
+
+				window.draw(s_map);
+			}
+
+		window.draw(p.sprite);
+		if (p.life == 0) {
+			text1.setString("Game over");
+
+		}
+		if (p.playerScore >= 200) {
+			text1.setString("You win");
+		}
+		std::ostringstream playerScoreString, playerHealthString, gameTimeString;
+		playerHealthString << p.health;
+		playerScoreString << p.playerScore;
+		gameTimeString << gameTime;
+		text.setString("Player score: " + playerScoreString.str() + "    Player health: "
+			+ playerHealthString.str() + "  Time in game: " + gameTimeString.str());
+		text.setPosition(view.getCenter().x - 600, view.getCenter().y - 300);//Р·Р°РґР°РµРј РїРѕР·РёС†РёСЋ С‚РµРєСЃС‚Р°, РѕС‚СЃС‚СѓРїР°СЏ РѕС‚ С†РµРЅС‚СЂР° РєР°РјРµСЂС‹
+		window.draw(text);
+		text1.setPosition(view.getCenter().x - 100, view.getCenter().y);
+		window.draw(text1);
+		window.display();
 	}
+	return 0;
+}
 
